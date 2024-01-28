1a40234685f6f82abc45dc74ec1a1b2464c173d5
Vanosultanishvili <148572577+Vanosultanishvili@users.noreply.github.com>
2024-01-29 01:28:57 +0400

    Add files via upload

---
+++ day 31/MEGA PROJECT PHYTHON/diceroller.py
@@ -0,0 +1,48 @@
+name = input("enter your name:")
+score = 0
+def rolldice():
+  import random
+  dice = random.randint(1,6)
+  print("computer rolled", dice)
+
+rolldice()
+
+def rolldice2():
+  import random
+  dice = random.randint(1,6)
+  print(name + " rolled", dice)
+
+rolldice2()
+
+def rolldice():
+  import random
+  dice = random.randint(1,6)
+  print("computer rolled", dice)
+
+rolldice()
+
+def rolldice2():
+  import random
+  dice = random.randint(1,6)
+  print(name + " rolled", dice)
+
+rolldice2()
+
+def rolldice():
+  import random
+  dice = random.randint(1,6)
+  print("computer rolled", dice)
+
+rolldice()
+
+def rolldice2():
+  import random
+  dice = random.randint(1,6)
+  print(name + " rolled", dice)
+
+rolldice2()
+
+
+
+
+
